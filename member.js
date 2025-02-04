47a7621a735bd791c68603274ebb1fdbc0c15270
Miguel Raziel Jesús Fernández Pascual <96704399+RazFernandez@users.noreply.github.com>
2025-02-04 22:41:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillMember() {
+    return {
+        "name": "member",
+        "type": "skill",
+        "description": "Members of the guild",
+        "options": [
+            {
+                "name": "list",
+                "type": 1,
+                "description": "List members of the guild",
+                "options": []
+            }
+        ]
+    };
+}
